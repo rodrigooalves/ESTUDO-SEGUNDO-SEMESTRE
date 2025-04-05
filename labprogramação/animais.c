4c4e6327397a1e8abba4abcb6accde680fcfce31
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-04-04 21:33:16 -0300

    site para estudar struct

---
+++ labprogramação/animais.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define TAM 3
+
+int main(){ 
+    int idade[TAM];
+    char sexo[TAM];
+
+    int sair = 0;
+    int opcao;
+    while(!sair) {
+        switch (){
+            case 1
+        }
+        
+        
+        
+        
+        
+        sair = 1;
+    }
+    
+    // fazendo a leitura dos dados
+    for (int i = 0; i < TAM; i++){
+        printf("Digite a idade: ");
+        scanf("%d", &idade[i]);
+        getchar();
+        printf("Digite a sexo: ");
+        scanf("%c", &sexo[i]);
+    }
+
+    //imprimindo todos
+    printf("#### Todos animais\n");
+    for (int i = 0; i < TAM; i++){
+        printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
+    }
+    
+    // calcular a média
+    int soma = 0;
+    for (int i = 0; i < TAM; i++){
+        soma += idade[i];
+    }
+    int media = soma / TAM;
+
+    //imprimindo maiores que a média
+    printf("#### Animais com idade acima da média\n");
+    for (int i = 0; i < TAM; i++){
+        if (idade[i] > media)
+            printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
+    }
+}
+//inf.pucrs.br/~pinho/Laprol/Structs/Structs.htm
