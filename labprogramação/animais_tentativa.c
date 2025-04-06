8e1b7a14e5f162a362112473cb049a1807c78a4f
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-04-05 21:35:02 -0300

    Codigo refatorado, corrigido erro no case 3, onde foi necessario declarar na variavel soma = 0. E no case 4, precisou ser feito um contador para validar

--- labprogramação/animais_tentativa.c
+++ labprogramação/animais_tentativa.c
@@ -2,23 +2,27 @@
 
 #include <stdio.h>
 #define TAM 3
+void cadastro() {
+    
+}
 
 int main () {
 
     int idade[TAM];
     char sexo[TAM];
-    int i,soma,media;
+    int i,media;
     int sair = 0;
     int opcao;
     int qtd = 0;
+    int soma = 0;
 
     while(!sair) { //sair == 0 OU sair != 1
-        printf("### Opções ###\n");
+        printf("\n### Opções ###\n");
         printf("### 1. Cadastro de animais ###\n");
         printf("### 2. Relatório com idade e sexo ###\n");
         printf("### 3. Relatorio com media dos animais ###\n");
         printf("### 4. Relatorio com idade maior que a media ###\n");
-        printf("### 0. SAIR  ###\n");
+        printf("### 0. SAIR  ###\n\n");
         scanf("%d", &opcao);
 
         switch (opcao) {
@@ -30,7 +34,7 @@ int main () {
                     printf("Digite o sexo do animal: \n");
                     scanf("%c", &sexo[qtd]);
                     qtd++;
-                    printf("Animal cadastrado com sucesso!\n");
+                    printf("Animal cadastrado com sucesso!\n\n");
                 }
                 else {
                     printf("Limite de animais cadastrados atingido.\n");
@@ -40,7 +44,7 @@ int main () {
                 case 2: //RELATORIO DOS ANIMAIS COM IDADE E SEXO :
                 printf("Imprimindo todos os animais\n");
                 for( i = 0; i < qtd; i++) {
-                    printf(" %d° Animal: Idade: %d , Sexo: %c\n", i+1,idade[qtd],sexo[qtd]);
+                    printf(" %d Animal: Idade: %d , Sexo: %c\n", i+1,idade[i],sexo[i]);
                 }   
                 break;
 
@@ -48,18 +52,22 @@ int main () {
                 for( i = 0; i < qtd; i++ ) {
                     soma += idade[i];
                 }
-                media = soma / 2;
-                printf("Media das idades: %d\n", media);
+                media = soma / qtd;
+                printf("Media das idades: %d\n\n", media);
                 break;
             
             case 4: // RELATORIO MEDIA MOSTRANDO OS QUE ESTÃO ACIMA DA MEDIA :
+                printf("Animais com idade acima da média:\n");
+                int found = 0;
                 for(i = 0; i < qtd; i++) {
                     if (idade[i] > media){
-                        printf("Idade %d - Sexo %c\n", idade[i], sexo[i]);
-                    } else {
-                        printf("Nao ha idades maiores que a media.\n");
+                        printf("Animal %d = Idade %d - Sexo %c\n",i+1, idade[i], sexo[i]);
+                        found = 1;
                     }
                 }
+                if(!found){
+                    printf("Nao ha idades maiores que a media.\n");
+                    }
                 break;
 
             case 0:
